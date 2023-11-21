069f50ec3e91f5aebd3eda6a91be7e9031cdff55
satya kumar <rambalasatya97@gmail.com>
2023-11-21 17:59:32 +0530

    dist related code removed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const path=require('path');
+// const path=require('path');
 const errorHandler = require('./middleware/errorHandler');
 const connectDb = require('./config/dbConnection');
 const cors=require('cors')
@@ -14,16 +14,16 @@ app.use(cors())
 app.use(express.json());  
 app.use(express.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname,'dist/employee-reports')));
+// app.use(express.static(path.join(__dirname,'dist/employee-reports')));
 
 app.use("/api/employee",require("./routes/employeeRoutes"));
 app.use("/api/users",require("./routes/userRoutes")); 
 app.use("/api/attendance",require("./routes/attendanceRoutes"))  ;
 app.use(errorHandler);
 
-app.use('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/employee-reports/index.html'));
-})
+// app.use('/*',function(req,res){
+//     res.sendFile(path.join(__dirname+'/dist/employee-reports/index.html'));
+// })
 
 app.listen(port,()=>{  
     console.log(`server running in port ${port}`);      
